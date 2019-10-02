fb2428fa6710e23f88072bac67b169ff018af428
Ben Barela <benitobarela@yahoo.com>
2019-10-02 07:57:08 -0500

    small card

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class Demo extends LightningElement {}
+export default class Demo extends LightningElement {
+    message = 'Non-Reactive Property';
+
+    handleChange(){
+        this.message = 'Changed to Reactive Property';
+    }
+}
